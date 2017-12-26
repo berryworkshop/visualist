3bd85fb578f255899a86a0e38ef76bb98e102b6a
Allan Berry <allan.berry@gmail.com>
2017-12-26 09:13:32 -0600

    database indexes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :events
+
+  scope '/api' do
+    resources :events
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
