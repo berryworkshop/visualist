5d972b13ea31ff23a82d92f52e503a257843755e
Allan Berry <allan.berry@gmail.com>
2018-01-03 17:30:03 -0600

    basic auth in place

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   before_action :set_page, only: [:show, :update, :destroy]
+  before_action :authenticate_user, except: [:index, :show]
 
   # GET /pages
   def index
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+admin = User.new
+admin.name = 'admin'
+admin.email = 'admin@example.com'
+admin.password = 'jehosoph'
+admin.password_confirmation = 'jehosoph'
+admin.admin = true
+admin.save
+
+user = User.new
+user.name = 'allan'
+user.email = 'user@example.com'
+user.password = 'jehosoph'
+user.password_confirmation = 'jehosoph'
+user.admin = false
+user.save
