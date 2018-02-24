0f8255e53ad5fae57a3046c10fbdf4ee1ab2ace2
Allan Berry <allan.berry@gmail.com>
2018-02-24 13:14:49 -0600

    association may be functional

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,20 +1,19 @@
 class Node < ApplicationRecord
   has_many :edges_out,
     class_name: 'Edge',
-    foreign_key: :subject,
+    foreign_key: :subject_id,
     dependent: :destroy
 
   has_many :edges_in,
     class_name: 'Edge',
-    foreign_key: :dobject,
+    foreign_key: :dobject_id,
     dependent: :destroy
 
-  # has_many :nodes_out,
-  #   through: :edges_out,
-  #   source: 'Edge'
+  has_many :nodes_out,
+    through: :edges_out,
+    source: :dobject
 
-
-  # has_many :nodes_in,
-  #   through: :edges_in,
-  #   source: :subject
+  has_many :nodes_in,
+    through: :edges_in,
+    source: :subject
 end
