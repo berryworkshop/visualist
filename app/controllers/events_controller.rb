91d335b339c5f085ace77ede4e038c1d2edc7c65
Allan Berry <allan.berry@gmail.com>
2018-03-05 10:21:12 -0600

    refactor events controller'

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,53 +1,13 @@
 require_relative '../serializers/node_serializer.rb'
 
-class EventsController < ApplicationController
-  before_action :set_event, only: [:show, :update, :destroy]
-
-  # GET /events
-  def index
-    @events = Event.all
-
-    render json: NodeSerializer.new(@events).serializable_hash
-  end
-
-  # GET /events/1
-  def show
-    render json: NodeSerializer.new(@event).serializable_hash
-  end
-
-  # POST /events
-  def create
-    @event = Event.new(attributes: event_params)
-
-    if @event.save
-      render json: NodeSerializer.new(@event).serializable_hash, status: :created, location: @event
-    else
-      render json: @event.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /events/1
-  def update
-    if @event.update(attributes: event_params)
-      render json: NodeSerializer.new(@event).serializable_hash
-    else
-      render json: @event.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /events/1
-  def destroy
-    @event.destroy
-  end
+class EventsController < NodesController
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_event
-      @event = Event.find(params[:id])
+    def set_node
+      @node = Event.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
-    def event_params
+    def node_params
       params.require(:attributes).permit(
         :type, :slug, :title, :body, :properties)
     end
--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -2,25 +2,26 @@ require_relative '../serializers/node_serializer.rb'
 
 class NodesController < ApplicationController
   before_action :set_node, only: [:show, :update, :destroy]
+  @@model = Node
 
   # GET /nodes
   def index
-    @nodes = Node.all
+    @nodes = @@model.all
 
-    render json: NodeSerializer.new(@nodes).serializable_hash
+    render json: serialize(@nodes)
   end
 
   # GET /nodes/1
   def show
-    render json: NodeSerializer.new(@node).serializable_hash
+    render json: serialize(@node)
   end
 
   # POST /nodes
   def create
-    @node = Node.new(node_params)
+    @node = @@model.new(node_params)
 
     if @node.save
-      render json: NodeSerializer.new(@node).serializable_hash, status: :created, location: @node
+      render json: serialize(@node), status: :created, location: @node
     else
       render json: @node.errors, status: :unprocessable_entity
     end
@@ -29,7 +30,7 @@ class NodesController < ApplicationController
   # PATCH/PUT /nodes/1
   def update
     if @node.update(node_params)
-      render json: NodeSerializer.new(@node).serializable_hash
+      render json: serialize(@node)
     else
       render json: @node.errors, status: :unprocessable_entity
     end
@@ -43,7 +44,11 @@ class NodesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_node
-      @node = Node.find(params[:id])
+      @node = @@model.find(params[:id])
+    end
+
+    def serialize(node)
+      NodeSerializer.new(node).serializable_hash
     end
 
     # Only allow a trusted parameter "white list" through.
