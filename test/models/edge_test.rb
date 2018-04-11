84c4e6b0089e8ca23feb1c2856a09f1365846005
Allan Berry <allan.berry@gmail.com>
2018-04-10 22:17:53 -0500

    progress

--- test/models/edge_test.rb
+++ test/models/edge_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class EdgeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @edge = edges(:allan_friend_meg)
+  end
+
+  test 'invalid without subject' do
+    @edge.subject = nil
+    refute @edge.valid?, 'edge is valid without a subject'
+    assert_not_nil @edge.errors[:name], 'no validation error for subject missing'
+  end
+
+  test 'invalid without predicate' do
+    @edge.predicate = nil
+    refute @edge.valid?, 'edge is valid without a predicate'
+    assert_not_nil @edge.errors[:name], 'no validation error for predicate missing'
+  end
+
+  test 'invalid without dobject' do
+    @edge.dobject = nil
+    refute @edge.valid?, 'edge is valid without a direct object'
+    assert_not_nil @edge.errors[:name], 'no validation error for direct object missing'
+  end
+
 end
